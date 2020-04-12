3e69fc57b8bb84d29339236f56978dbd0fc61de0
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-04-12 16:17:09 +0300

    Delete django-dotenv and install python-dotenv

--- manage.py
+++ manage.py
@@ -2,11 +2,11 @@
 import os
 import sys
 
-import dotenv
+from dotenv import load_dotenv
 
 
 def main():
-    dotenv.read_dotenv()
+    load_dotenv()
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
