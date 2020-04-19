e46d766aa7b498462408bd368d556f28f7a1d464
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-04-19 18:43:18 +0300

    Customize user

--- accounts/models.py
+++ accounts/models.py
@@ -16,5 +16,9 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
+    class Meta(object):
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
     def __str__(self):
         return self.email
