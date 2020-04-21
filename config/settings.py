ac694277b963e524f60062e6bce73c08566f1411
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-04-21 20:45:41 +0300

    Refactor views and add logout

--- config/settings.py
+++ config/settings.py
@@ -99,6 +99,8 @@ if os.getenv('DATABASE_URL'):
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+LOGOUT_REDIRECT_URL = '/'
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
