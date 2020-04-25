c4e0c47011e87cbea4b8922f01fca787c280df0b
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-04-25 20:54:29 +0300

    Customize password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,28 @@
 from django.urls import path, include
+from django.contrib.auth.views import PasswordChangeDoneView
 
-from accounts.views import RegistrationView, profile
+from accounts.views import (
+    RegistrationView,
+    profile,
+    CustomPasswordChangeView,
+)
 
 
 app_name = 'accounts'
 urlpatterns = [
+    path(
+        'password_change/',
+        CustomPasswordChangeView.as_view(),
+        name='password_change'
+    ),
+    path(
+        'password_change/done/',
+        PasswordChangeDoneView.as_view(
+            template_name='accounts/password_change_done.html'
+        ),
+        name='password_change_done'
+    ),
     path('', include('django.contrib.auth.urls')),
     path('registration/', RegistrationView.as_view(), name='registration'),
-    path('accounts/profile/', profile, name='profile')
+    path('accounts/profile/', profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.shortcuts import render, redirect
+from django.contrib.auth.views import PasswordChangeView
 
 from accounts.forms import CustomUserCreationForm
 
@@ -9,6 +10,11 @@ class RegistrationView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('tasks:index')
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    template_name = 'accounts/password_change.html'
+    success_url = reverse_lazy('accounts:password_change_done')
 
 
 def profile(request):
