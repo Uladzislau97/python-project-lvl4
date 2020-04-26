219976e64e656655bf147ed5ce75d787bfe1aece
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-04-26 18:54:58 +0300

    Add TaskStatus, Tag and Task models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import (
+    TaskStatus,
+    Tag,
+    Task,
+)
+
+
+admin.site.register(TaskStatus)
+admin.site.register(Tag)
+admin.site.register(Task)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import CustomUser
+
+
+class TaskStatus(models.Model):
+    name = models.CharField(
+        blank=False,
+        max_length=255,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Task Statuses'
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        blank=False,
+        unique=True,
+        max_length=255,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(
+        blank=False,
+        max_length=255,
+    )
+    description = models.TextField(
+        blank=True
+    )
+    status = models.ForeignKey(
+        TaskStatus,
+        on_delete=models.PROTECT,
+        blank=False,
+    )
+    creator = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        blank=False,
+        related_name='created_tasks',
+    )
+    assigned_to = models.ForeignKey(
+        CustomUser,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='assigned_tasks',
+        blank=True
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
