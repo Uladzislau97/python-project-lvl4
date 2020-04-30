053b6d41ac41737db90e987e846bf7d176e7af0c
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-04-30 17:27:58 +0300

    Add list views for statuses and tags

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,13 +1,27 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from tasks.views import TaskListView
+from tasks.views import (
+    TaskListView,
+    TaskStatusListView,
+    TagListView,
+)
 
 app_name = 'tasks'
 urlpatterns = [
     path(
         '',
         login_required(TaskListView.as_view()),
-        name='task_list'
+        name='task_list',
+    ),
+    path(
+        'statuses/',
+        login_required(TaskStatusListView.as_view()),
+        name='task_status_list',
+    ),
+    path(
+        'tags/',
+        login_required(TagListView.as_view()),
+        name='tag_list',
     ),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,22 @@
 from django.views.generic.list import ListView
 
-from tasks.models import Task
+from tasks.models import (
+    Task,
+    TaskStatus,
+    Tag,
+)
 
 
 class TaskListView(ListView):
     model = Task
     template_name = 'tasks/task_list.html'
+
+
+class TaskStatusListView(ListView):
+    model = TaskStatus
+    template_name = 'tasks/task_status_list.html'
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = 'tasks/tag_list.html'
